ed66ccc4e2d95029ab3e1469c5dc33ac1443c7e6
wkitsin <wkitsin94@gmail.com>
2022-05-21 16:17:45 +0800

    Added contact and EditContact Screen

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Contacts from './Contacts';
+import EditContact from './EditContact';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Contacts}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={EditContact} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;

a5360bf8ea0bb0fbd69ad7866f391e0a9e5fa293
wkitsin <wkitsin94@gmail.com>
2022-05-22 17:19:32 +0800

    end commit

--- src/Contacts.tsx
+++ src/Contacts.tsx
@@ -1,13 +1,14 @@
-import { useFocusEffect } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React, { useContext, useEffect } from 'react';
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { FlatList, RefreshControl, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ContactContext } from './App';
-import { useGetContacts } from './helpers/asyncStorage';
+import { ASYNC_STORAGE_KEY, useGetContacts } from './helpers/asyncStorage';
 import { Color } from './helpers/color';
 import { SPACING } from './helpers/spacing';
 import { ContactListType, RootStackParamList } from './typings';
 import Icon from 'react-native-vector-icons/dist/Feather';
+import CONTACT_LISTS from './data.json';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ItemSeparatorComponent = () => (
   <View
@@ -22,6 +23,7 @@ type ContactsNavProps = NativeStackScreenProps<RootStackParamList, 'Contacts'>;
 const Contacts = (props: ContactsNavProps) => {
   const { navigation } = props;
   const { setContacts, contacts } = useContext(ContactContext);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   useEffect(() => {
     useGetContacts().then(result => {
@@ -57,9 +59,21 @@ const Contacts = (props: ContactsNavProps) => {
     </TouchableOpacity>
   )
 
+  const onRefresh = React.useCallback(() => {
+    setIsRefreshing(true);
+    setContacts(CONTACT_LISTS);
+    const jsonValue = JSON.stringify(CONTACT_LISTS);
+    AsyncStorage.setItem(ASYNC_STORAGE_KEY, jsonValue);
+    setIsRefreshing(false);
+  }, []);
+
   return (
     <View>
       <FlatList
+        refreshControl={<RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+        />}
         contentContainerStyle={styles.flatList}
         data={contacts}
         renderItem={renderItem}
